6c35793655cc2599eba92447e1f68b214cd2134d
DeveloperAromal <developeraromal@gmail.com>
2026-09-21 19:21:38 +0530

    feat: removed a func from visitors

--- backend/internal/features/visitor/helpers.go
+++ backend/internal/features/visitor/helpers.go
@@ -4,9 +4,6 @@ import (
 	"encoding/xml"
 	"os"
 	"strings"
-
-	"github.com/gin-gonic/gin"
-	"github.com/twilio/twilio-go"
 )
 
 func createVoiceTwiML(phoneNumber string) (string, error) {
@@ -30,27 +27,4 @@ func formatPhoneNumber(number string) string {
 	}
 
 	return "+91" + number
-}
-
-func isValidTwilioVoiceWebhook(c *gin.Context) bool {
-	voiceURL := os.Getenv("TWILIO_VOICE_URL")
-	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
-	signature := c.GetHeader("X-Twilio-Signature")
-	if voiceURL == "" || authToken == "" || signature == "" {
-		return false
-	}
-
-	if err := c.Request.ParseForm(); err != nil {
-		return false
-	}
-
-	params := make(map[string]string, len(c.Request.PostForm))
-	for key, values := range c.Request.PostForm {
-		if len(values) > 0 {
-			params[key] = values[0]
-		}
-	}
-
-	validator := twilio.NewRequestValidator(authToken)
-	return validator.Validate(voiceURL, params, signature)
 }
